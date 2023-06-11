0c065c35cdf88c6f42df465ddd2c2c642afa5a58
Ajay <81603467+Ajay1812@users.noreply.github.com>
2023-06-11 11:36:56 +0530

    Update app.py

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ def load_lottieurl(url):
 
 # --- Use Local CSS --- #
 def local_css(file_name):
-    with open(file_name, 'r') as f:
+    with open(file_name) as f:
         st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
 
 local_css('style/style.css')
